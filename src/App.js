3a6ad2649d4a91606782b25b22666ac309a0ee36
misbahs100 <misbahuddins100@gmail.com>
2021-06-09 10:25:16 +0600

    added gallery

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Header from './components/Header';
 import Features from './components/Features';
 import Hero from './components/Hero';
 import Testimonial from './components/Testimonial';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Header />
+      <Gallery />
       <Hero />
+      
       <Features />
       <Testimonial />
     </div>
